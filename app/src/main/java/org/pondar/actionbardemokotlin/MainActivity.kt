7d2ce6ff7ef6bb480004d7ad88a5e301232b77c2
Martin Knudsen <martinknud@gmail.com>
2021-10-29 12:58:24 +0200

    upgraded to viewbinding

--- app/src/main/java/org/pondar/actionbardemokotlin/MainActivity.kt
+++ app/src/main/java/org/pondar/actionbardemokotlin/MainActivity.kt
@@ -6,12 +6,19 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import org.pondar.actionbardemokotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+
+    lateinit var binding : ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 
 
